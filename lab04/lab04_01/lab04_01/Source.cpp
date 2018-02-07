642e8ba5add8cf5fd11d861e0f846d862853f9e5
ArthurIVA <submail.arthuriva@gmail.com>
2018-02-07 00:31:14 -0500

    Add Lab 04 to repository
    Not the entire project initially included since it was incomplete. Did not include gdoc and docx files for continued security purposes.

---
+++ lab04/lab04_01/lab04_01/Source.cpp
@@ -0,0 +1,53 @@
+/*
+*Author:			Arthur Aigeltinger IV
+*Creation Date:		02/06/18
+*Modification Date:	02/06/18
+*Purpose: Get temp, GIVE JUGDEMENT!
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int tempF; //Would use float if the threshhold needed to be more precise.
+			   // i.e. inputs like .3 will round down to 0, therefore technically in wrong category.
+	cout << "Please enter the current temperature (F) : ";
+	cin >> tempF;
+	cout << endl;
+
+	if (tempF > 90)
+	{
+		cout << "Go swimming.";
+	}
+	else if (tempF > 80)
+	{
+		cout << "Turn on air conditioning.";
+	}
+	else if (tempF > 70)
+	{
+		cout << "Do nothing.";
+	}
+	else if (tempF > 55)
+	{
+		cout << "Turn on heat.";
+	}
+	else if (tempF > 30)
+	{
+		cout << "Wear a heavy coat.";
+	}
+	else if (tempF > 0 )
+	{
+		cout << "Wear gloves";
+	}
+	else if (tempF <= 0) //kept final else if to handle extra cases.
+	{
+		cout << "Stay inside, make a fire.";
+	}
+
+	cout << endl << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ lab04/lab04_02/lab04_02/Source.cpp
@@ -0,0 +1,26 @@
+/*
+*Author:			Arthur Aigeltinger IV
+*Creation Date:		02/06/18
+*Modification Date:	02/06/18
+*Purpose: Get letter grade, GIVE MORE JUGDEMENT!
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char grade;
+
+	switch(grade)
+	{
+	case 'A':
+	case 'a':
+			cout << "Excellent" << endl;
+			break;
+	}
+
+	system("pause");
+	return 0;
+}
