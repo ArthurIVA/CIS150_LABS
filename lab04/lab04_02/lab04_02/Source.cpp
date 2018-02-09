85e485245a59f8ba318e47a9c1c84755b4e96ef4
ArthurIVA <submail.arthuriva@gmail.com>
2018-02-09 00:16:57 -0500

    Completed Switch in 0402
    Discovered cctype including function toupper to handle all lower case
    inputs without double the cases. Will need to take snippets to add to
    report.

--- lab04/lab04_02/lab04_02/Source.cpp
+++ lab04/lab04_02/lab04_02/Source.cpp
@@ -1,26 +1,69 @@
 /*
 *Author:			Arthur Aigeltinger IV
 *Creation Date:		02/06/18
-*Modification Date:	02/06/18
+*Modification Date:	02/08/18
 *Purpose: Get letter grade, GIVE MORE JUGDEMENT!
 */
 
 #include <iostream>
+#include <cctype> //Will help with exception handling in terms of UPPER and lower case inputs & invalid grade
 
 using namespace std;
 
 int main()
 {
-	char grade;
+	char grade = 0;
 
-	switch(grade)
+	cout << "Please enter the letter grade of the student: ";
+	cin >> grade;
+	grade = toupper(grade); //Applied toupper to handle all lower case inputs.
+
+	switch (grade)
 	{
-	case 'A':
-	case 'a':
+		case 'A':
+		{
 			cout << "Excellent" << endl;
 			break;
+		}
+		
+		case 'B':
+		{
+			cout << "Good" << endl;
+			break;
+		}
+
+		case 'C':
+		{
+			cout << "Average" << endl;
+			break;
+		}
+
+		case 'D':
+		{
+			cout << "Below Average" << endl;
+			break;
+		}
+
+		case 'E':
+		{
+			cout << "Below Average " << endl;
+			break;
+		}
+
+		case 'F':
+		{
+			cout << "Fail" << endl;
+			break;
+		}
+
+		default:
+		{
+			cout << "Invalid Grade" << endl;
+			break;
+		}
 	}
 
 	system("pause");
 	return 0;
+
 }
