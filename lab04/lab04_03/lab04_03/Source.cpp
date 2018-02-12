f5f9f4feeadeaa65e39bc2b96c2d8d65421db98d
ArthurIVA <submail.arthuriva@gmail.com>
2018-02-11 20:51:14 -0500

    Completed 0403
    This included report and snippets for question.

--- lab04/lab04_03/lab04_03/Source.cpp
+++ lab04/lab04_03/lab04_03/Source.cpp
@@ -1,6 +1,28 @@
 /*
 *Author:			Arthur Aigeltinger IV
 *Creation Date:		02/07/18
-*Modification Date:	02/07/18
-*Purpose: TO BE DETERMINED
+*Modification Date:	02/11/18
+*Purpose: Add and Iterate in different orders with ++ operators.
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int w = 20, x = 20;
+	int y = 5, z = 5;
+
+	y = y + (++x);
+	z = z + (w++);
+
+	cout << "x=" << x << " and y=" << y << endl;
+	cout << "w=" << w << "and z=" << z << endl;
+
+
+	system("pause");
+	return 0;
+
+}
