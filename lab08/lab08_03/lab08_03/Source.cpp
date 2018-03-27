9149be6b7885d621c77e8fefe7e1159b17c6b2b4
ArthurIVA <submail.arthuriva@gmail.com>
2018-03-27 16:11:38 -0400

    Drafted First Version of 0803

--- lab08/lab08_03/lab08_03/Source.cpp
+++ lab08/lab08_03/lab08_03/Source.cpp
@@ -4,3 +4,72 @@
 *Modification Date:	03/27/18
 *Purpose: 
 */
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	struct letterOccur
+	{
+		char letter;
+		unsigned short occurrences;
+	};
+
+	string holder[10];
+	vector<letterOccur> characterHolder;
+	ifstream file("poem.txt");
+	ofstream capitalOutput("capitalOutput.txt");
+	ofstream statisticsOutput("statisticsOutput.txt");
+
+	for (unsigned short i = 0; i < 10; i++)  //Loop to pass lines from file into the array.
+	{
+		getline(file, holder[i]); 
+	}	
+
+	file.seekg(0);
+
+	while (!file.eof())
+	{
+		letterOccur *tempObject = new letterOccur;
+		bool letterExists = false;
+		unsigned short posOfExistingLetter = 0;
+		for (unsigned short i = 0; i < characterHolder.size(); i++)
+		{
+			if (toupper(file.peek()) == characterHolder.at(i).letter)
+			{
+				letterExists = true;
+				posOfExistingLetter = i;
+				break;
+			}
+		}
+
+		if (letterExists == true)
+			characterHolder.at(posOfExistingLetter).occurrences++;
+
+		else if (letterExists == false && file.peek() != '32' && file.peek() != -1 && file.peek() != '\n')
+		{
+			tempObject->letter = toupper(file.peek());
+			tempObject->occurrences = 1;
+			characterHolder.push_back(*tempObject);
+		}
+
+		file.get();
+		letterExists = false;
+	}
+
+	for (int i = 0; i < characterHolder.size(); i++)
+	{
+		cout << "Letter: " << characterHolder.at(i).letter << endl;
+		cout << "Occurrences: " << characterHolder.at(i).occurrences << endl;
+		cout << endl;
+	}
+
+
+
+	system("PAUSE");
+}
