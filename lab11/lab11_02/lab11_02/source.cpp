7aa9ec740e15ba216a7b61735bdc7ad61e63739b
ArthurIVA <submail.arthuriva@gmail.com>
2018-04-08 19:28:03 -0400

    Created 1102
    Will move forward with 1102 asap.

---
+++ lab11/lab11_02/lab11_02/source.cpp
@@ -0,0 +1,17 @@
+/*
+*Author: Arthur Aigeltinger IV
+*Creation Date:		04/08/18
+*Modification Date:	04/08/18
+*Purpose: 
+*/
+
+#include <iostream>
+
+
+main()
+{
+
+
+	system("pause");
+	return 0;
+}
---
+++ lab11/lab11_02/lab11_02/student.h
@@ -0,0 +1,13 @@
+#pragma once
+class student
+{
+private:
+
+
+
+
+public:
+	student();
+	~student();
+};
+
