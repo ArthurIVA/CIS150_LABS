bfe25f79e755f6311e380c2ba0ebd34d267d643c
Arthur Aigeltinger IV <submail.arthuriva@gmail.com>
2018-04-10 18:34:25 -0400

    Created Project 02

---
+++ project02/airplaneSeating/airplaneSeating/source.cpp
@@ -0,0 +1,12 @@
+/*
+Author: Arthur Aigeltinger IV
+Creation Date:		04/10/18
+Modification Date:	04/10/18
+Purpose:
+*/
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+//using namespace std; NOT NEEDED CURRENTLY
